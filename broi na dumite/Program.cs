bdb3d35808e9813e52e4f9bafad6f6a27e01a711
User 12 <User 12@PC17>
2021-10-27 14:40:53 +0300

    Add project files.

---
+++ broi na dumite/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace broi_na_dumite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int wordCount = 0, index = 0;
+
+            
+            while (index < text.Length && char.IsWhiteSpace(text[index]))
+                index++;
+
+            while (index < text.Length)
+            {
+                
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+                
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+               
+            }
+            Console.WriteLine(wordCount);
+        }
+    }
+}
